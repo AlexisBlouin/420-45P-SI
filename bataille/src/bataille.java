97521513d1e35d7ed43d1eafd23ae5dd18874f2c
AlexisBlouin <blouin.alexis@icloud.com>
2023-02-28 22:18:47 -0500

    Affichage durant partie terminé

--- bataille/src/bataille.java
+++ bataille/src/bataille.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
  * Programme bataille.java permettant de jouer au jeu Battleship contre un ordinateur.
  * @author AlexisBlouin
  * @since 6 février 2023
+ * @remise 6 mars 2023
  */
 public class bataille {
     public static int [][]grilleOrdi = new int[10][10];
@@ -30,9 +31,8 @@ public class bataille {
      */
     public static void engagement(){
         initGrilleOrdi();
-        afficherGrille(grilleOrdi);
         initGrilleJeu();
-        afficherGrille(grilleJeu);
+        System.out.println("Votre grille est maintenant complete.");
 
         while(!partieFinie){
 
@@ -123,6 +123,9 @@ public class bataille {
             }
 
             ecrireDansGrille(grilleJeu, ligne, colonne, direction, grandeurBateau, numeroBateau);
+
+            System.out.println("Vous avez reussi a ajouter le " + nomBateau[numeroBateau - 1]);
+            afficherGrille(grilleJeu);
         }
     }
 
@@ -162,9 +165,7 @@ public class bataille {
             colonneChar = Character.toUpperCase(colonneChar);
         }
         colonneInt = colonneChar;
-        System.out.println(colonneInt);
         colonneInt -= 65;
-        System.out.println(colonneInt);
         return colonneInt;
     }
 
@@ -388,6 +389,7 @@ public class bataille {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
     /**
@@ -399,13 +401,18 @@ public class bataille {
     public static void tirOrdinateur(){
         int l;
         int c;
+        char colonneChar;
         l = randRange(0, 10);
         c = randRange(0, 10);
-        System.out.println("l : " + l + " c : " + c);
+        colonneChar = (char)(c + 65);
+        System.out.println('\n' + "C'est au tour de l'ordinateur." + '\n');
         System.out.println("Tir de l'ordinateur : ");
-        System.out.println("C'est au tour de l'ordinateur.");
+        System.out.println("Colonne : " + colonneChar + ", Ligne : " + l + '\n');
         mouvement(grilleJeu, l, c);
-        afficherGrille(grilleJeu);
+        if(!partieFinie){
+            System.out.println('\n' + "Votre grille apres le tir : ");
+            afficherGrille(grilleJeu);
+        }
     }
 
     /**
@@ -416,15 +423,18 @@ public class bataille {
      * </pre>
      */
     public static void tirJoueur(){
-        int colonne;
-        int ligne;
+        int c;
+        int l;
+        char colonneChar;
 
-        System.out.println("C'est votre tour!");
-        colonne = demanderColonne(false, 0);
-        ligne = demanderLigne(false, 0);
+        System.out.println("C'est votre tour!" + '\n');
+        c = demanderColonne(false, 0);
+        l = demanderLigne(false, 0);
+        colonneChar = (char)(c + 65);
 
-        mouvement(grilleOrdi, ligne, colonne);
-        afficherGrille(grilleOrdi);
+        System.out.println("Votre tir : ");
+        System.out.println("Colonne : " + colonneChar + ", Ligne : " + l + '\n');
+        mouvement(grilleOrdi, l, c);
     }
 
     /**
@@ -461,15 +471,23 @@ public class bataille {
                 if(vainqueur(grille)){
 
                     if(grille == grilleOrdi){
-                        System.out.println("Vous avez gagne!");
+                        System.out.println('\n' + "Vous avez gagne!" + '\n');
+                        System.out.println("Tous les bateaux ennemis sont coules : ");
+                        afficherGrille(grilleOrdi);
+                        System.out.println("Voici ce qu'il reste de votre grille : ");
+                        afficherGrille(grilleJeu);
                     }
                     else{
-                        System.out.println("Vous avez perdu...");
+                        System.out.println('\n' + "Vous avez perdu..." + '\n');
+                        System.out.println("Tous vos bateaux sont coule : ");
+                        afficherGrille(grilleJeu);
+                        System.out.println("Voici ce qu'il reste de la grille ennemie : ");
+                        afficherGrille(grilleOrdi);
                     }
                     partieFinie = true;
                 }
                 else{
-                    System.out.println("Pas gagne...");
+                    System.out.println("Tous les bateaux ne sont pas coules." + '\n');
                 }
             }
         }
