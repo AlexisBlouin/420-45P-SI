6347910c1cdb16bc52feae1519c90741b161c217
AlexisBlouin <blouin.alexis@icloud.com>
2023-03-21 09:20:15 -0400

    ProjetRetire

--- batailleFx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.bataillefx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.bataillefx to javafx.fxml;
-    exports com.example.bataillefx;
-}
