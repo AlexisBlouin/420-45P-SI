76316fbba0c8efd19356cd6083d9e0ec066d24d1
AlexisBlouin <blouin.alexis@icloud.com>
2023-03-22 16:49:14 -0400

    Capable de poser image dans GridPane

--- batailleFx/src/main/java/com/example/bataillefx/HelloApplication.java
+++ batailleFx/src/main/java/com/example/bataillefx/HelloApplication.java
@@ -23,6 +23,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ScenePlacementBateaux.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+        stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- batailleFx/src/main/java/com/example/bataillefx/HelloController.java
+++ batailleFx/src/main/java/com/example/bataillefx/HelloController.java
@@ -1,12 +1,16 @@
 package com.example.bataillefx;
 
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import java.io.IOException;
@@ -17,11 +21,29 @@ import javafx.scene.image.ImageView;
 public class HelloController {
     @FXML
     public GridPane grilleJoueur;
+    public ImageView porteAvions;
+    public ImageView torpilleur;
 
-    public void clickGrid(javafx.scene.input.MouseEvent event) {
-        Node source = (Node)event.getSource() ;
-        Integer colIndex = grilleJoueur.getColumnIndex(source);
-        Integer rowIndex = grilleJoueur.getRowIndex(source);
-        System.out.println("Mouse clicked cell: " + colIndex + "And: " + rowIndex);
+    public void clickGrid(javafx.scene.input.MouseEvent t) {
+        Node source = (Node)t.getSource();
+        System.out.println("X : " + t.getX() + ", Y : " + t.getY());
+        Integer row = GridPane.getRowIndex(source);
+        Integer column = GridPane.getColumnIndex(source);
+        System.out.println("L : " + row + ", C : " + column);
+
+        Rectangle carreBleu = new Rectangle(0, 0, grilleJoueur.getWidth()/11, grilleJoueur.getHeight()/11);
+        carreBleu.setFill(Color.CADETBLUE);
+
+        //grilleJoueur.add(carreBleu, column, row);
+        grilleJoueur.add(porteAvions, column, row);
+    }
+
+    public void tournerBateau(){
+        if(porteAvions.getRotate() == 0){
+            porteAvions.setRotate(90);
+        }
+        else {
+            porteAvions.setRotate(0);
+        }
     }
 }
